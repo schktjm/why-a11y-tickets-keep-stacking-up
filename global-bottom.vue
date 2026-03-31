5039d28a48c4e26cba11bf83de6c40e50a9fa6fc
schktjm <schktjm@gmail.com>
2026-03-31 21:29:00 +0900

    feat: slidev完全に理解した

---
+++ global-bottom.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="absolute left-0 inset-y-0 w-16 cursor-pointer"
+    style="z-index: 100"
+    @click="$slidev.nav.prev()"
+  />
+  <div
+    class="absolute right-0 inset-y-0 w-16 cursor-pointer"
+    style="z-index: 100"
+    @click="$slidev.nav.next()"
+  />
+  <div class="absolute bottom-12 right-4 text-sm opacity-70" style="z-index: 100">
+    {{ $slidev.nav.currentPage }}
+  </div>
+</template>
