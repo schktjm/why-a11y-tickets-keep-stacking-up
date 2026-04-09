2b221243df4fa11657951c0120dcc78a408c5d79
schktjm <schktjm@gmail.com>
2026-04-09 18:51:52 +0900

    fix

---
+++ components/TaskCost.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex text-[1.5rem] ![&_h3]:text-[1.5rem] ![&_h3]:mb-2">
+        <div class="grow-4 bg-cyan-50 dark:bg-cyan-800 border-8 border-cyan-500 p-4 rounded-l-lg">
+            <h3>80%: メインの仕事</h3>
+            <div>新しい機能をユーザーに提供する</div>
+        </div>
+        <div class="grow-1 bg-pink-50 dark:bg-pink-800 border-8 border-pink-500 p-4 rounded-r-lg">
+            <h3>20%: その他</h3>
+            <div>
+                <ul class="![&_li]:text-[1.2rem]">
+                    <li>AI Agent</li>
+                    <li>セキュリティ</li>
+                    <li>パフォーマンス</li>
+                    <li>アクセシビリティ</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
